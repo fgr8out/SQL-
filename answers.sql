8c5cd8f42e488933490cc78690349b675467b9c0
Hackbright Student <info@hackbrightacademy.com>
2015-10-12 16:29:14 -0700

    second commit for SQL quiz

--- answers.sql
+++ answers.sql
@@ -110,3 +110,94 @@ anywhere in the common name.
 
 
 Select common_name From melons Where common_name Like '%Golden%';
+
+
+==========
+9
+
+-----
+
+Write a query that shows all the distinct regions that a salesperson can belong to.
+
+
+-----
+
+
+SELECT DISTINCT region FROM salespeople
+;
+
+
+==========
+10
+
+-----
+
+Write a query that shows the emails of all salespeople from both the
+Northwest and Southwest regions.
+
+
+-----
+
+
+SELECT email FROM salespeople WHERE region = 'Northwest' OR region = 'Southwest'
+;
+
+
+==========
+11
+
+-----
+
+Write a query that shows the emails of all salespeople from both the
+Northwest and Southwest regions, this time using an 'IN' clause.  
+
+
+-----
+
+
+SELECT email FROM salespeople WHERE region IN ('Northwest', 'Southwest');
+
+
+==========
+12
+
+-----
+
+Write a query that shows the email, given name, and surname of all
+salespeople in either the Northwest or Southwest regions whose surnames start
+with the letter 'M'.
+
+-----
+
+
+SELECT email, givenname, surname FROM salespeople WHERE region IN ('Northwest', 'Southwest') AND surname LIKE 'M%';
+
+
+==========
+13
+
+-----
+
+Write a query that shows the melon type, common name, price, and the
+price of the melon given in euros. The 'melons' table has prices in dollars,
+and the dollar to euro conversion rate is 0.735693.
+
+
+-----
+
+
+SELECT melon_type, common_name, price, price * 0.735693 FROM melons;
+
+
+==========
+14
+
+-----
+
+Write a query that shows the total number of customers in our customer
+table.
+
+-----
+
+
+Select COUNT(*) from customers;
